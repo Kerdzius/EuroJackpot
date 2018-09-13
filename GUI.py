0247c9211b6793c42043cf50c5563ab19dba1ee6
Jonas <jonas.stonys@hotmail.com>
2018-09-13 16:04:49 +0100

    Testing changes
    Testing changes

--- GUI.py
+++ GUI.py
@@ -1,81 +1,91 @@
 #!/usr/bin/env python4
 from tkinter import *
 from tkinter import ttk
-from EuroJackpot import EuroJackpotWebScrap
+from euroJackpot import euroJackpotWebScrap
 from tkinter import messagebox
+from time import sleep
 
 
 class GUIforEuroJackpotWeb:
-    
+
     def __init__(self):
         self.window = Tk()
-        
-        
+
         self.nrFrom = StringVar()
         self.nrTo = StringVar()
-        
-    
+
         self.theEntries()
         self.theButton()
-        
-        
-        self.newWebScrap = EuroJackpotWebScrap()
+
+        self.newWebScrap = euroJackpotWebScrap()
         self.currentMaximum = self.newWebScrap.currentMax()
-        self.theLabels()
-        
+
+        li1 = Label(self.window, text="Current minimum: 1")
+        li1.grid(row=0, column=0)
+        li2 = Label(self.window, text="From:")
+        li2.grid(row=1, column=0)
+        li3 = Label(self.window, text="To:")
+        li3.grid(row=1, column=2)
+        li4 = Label(self.window, text="Current maximum:")
+        li4.grid(row=0, column=2)
+        li5 = Label(self.window, text=self.currentMaximum)
+        li5.grid(row=0, column=3)
+        li6 = Label(self.window, text='Current Status:')
+        li6.grid(row=3, column=0)
+        li7 = Label(self.window, text='Waiting for input')
+        li7.grid(row=3, column=1)
+
         self.window.mainloop()
-    
-    def theLabels(self):
-        li = Label(self.window, text = "Current minimum: 1")
-        li.grid(row = 0, column = 0)
-        li = Label(self.window, text = "From:")
-        li.grid(row = 1, column = 0)
-        li = Label(self.window, text = "To:")
-        li.grid(row = 1, column = 2)
-        li = Label(self.window, text = "Current maximum:")
-        li.grid(row = 0, column = 2)
-        li = Label(self.window, text = self.currentMaximum)
-        li.grid(row = 0, column = 3)
-        
+
     def theEntries(self):
-        entryFrom = Entry (self.window, textvariable = self.nrFrom)
-        entryFrom.grid(row = 1, column = 1)
-              
-        entryTo = Entry (self.window, textvariable = self.nrTo)
-        entryTo.grid(row = 1, column = 3)
-        
-            
+        entryFrom = Entry(self.window, textvariable=self.nrFrom)
+        entryFrom.grid(row=1, column=1)
+
+        entryTo = Entry(self.window, textvariable=self.nrTo)
+        entryTo.grid(row=1, column=3)
+
     def theButton(self):
-        b1 = Button(self.window, text = "Run", command = self.buttonExecute)
-        b1.grid(row = 3, column = 3)
-        
-        
+        b1 = Button(self.window, text="Run", command=self.buttonExecute)
+        b1.grid(row=3, column=3)
+
     def buttonExecute(self):
+        check = self.GUIchecks()
+        if check == True:
+            textNrFrom = self.nrFrom.get()
+            textNrTo = self.nrTo.get()
+            self.newWebScrap.coord(textNrFrom, textNrTo)
+            messagebox.showinfo("Title", "The information has been gathered successfully")
+
+    def GUIchecks(self):
+        GUIReturn = False
         try:
             textNrFrom = self.nrFrom.get()
             textNrTo = self.nrTo.get()
-
-            if (int(textNrFrom) < 1):
-                messagebox.showinfo("Title", "The entered number in the field \"From:\" is less than the first recorded draw")    
+            if int(textNrFrom) < 1:
+                messagebox.showinfo("Title", "The entered number in the"
+                                             " field \"From:\" is less than the first recorded draw")
             else:
-                if (int(textNrFrom) > int(self.currentMaximum)):
-                    messagebox.showinfo("Title", "The entered number in the field \"From:\" is higher than the last recorded draw")
+                if int(textNrFrom) > int(self.currentMaximum):
+                    messagebox.showinfo("Title", "The entered number in the field \"From:\" is "
+                                                 "higher than the last recorded draw")
                 else:
-                    if (int(textNrTo) > int(self.currentMaximum)):
-                        messagebox.showinfo("Title", "The entered number in the field \"To:\" is higher than the last recorded draw")
+                    if int(textNrTo) > int(self.currentMaximum):
+                        messagebox.showinfo("Title", "The entered number in the field \"To:\" is"
+                                                     " higher than the last recorded draw")
                     else:
-                        if(int(textNrFrom)> int(textNrTo)):
-                            messagebox.showinfo("Title", "The entered number in the field \"From:\" is higher than a number in the field \"To:\"")
+                        if int(textNrFrom) > int(textNrTo):
+                            messagebox.showinfo("Title", "The entered number in the field \"From:\" is"
+                                                         " higher than a number in the field \"To:\"")
                         else:
-                            if(self.newWebScrap.testTextFile() == False):
-                                messagebox.showinfo("Title", "The file \"myData.csv\" is currently open. Please Close it before continuing ")
+                            if self.newWebScrap.testTextFile() == False:
+                                messagebox.showinfo("Title", "The file \"myData.csv\" is currently open."
+                                                             " Please Close it before continuing ")
                             else:
-                                self.newWebScrap.coord(textNrFrom, textNrTo)
-                                messagebox.showinfo("Title", "The information has been gathered successfully")
-                        
+                                GUIReturn = True
+
         except ValueError:
-                messagebox.showinfo("Error", "Not a number has been entered!")
-            
+            messagebox.showinfo("Error", "Not a number has been entered!")
+        return GUIReturn
 
 
 a = GUIforEuroJackpotWeb()
